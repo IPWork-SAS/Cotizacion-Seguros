6c6948cc707e11fed9d7e4a388a5d21e7174065a
Duvan Gonzalez <programmerloquillo@gmail.com>
2019-10-08 11:56:40 -0500

    Se agrega validacion para pasar a produccion

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .js('resources/js/scriptspropios.js', 'public/js');
+
+if (mix.config.inProduction) {
+    mix.version();
+    mix.minify();
+}
