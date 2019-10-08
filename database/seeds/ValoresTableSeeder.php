deb0d1ce1d59ecfca24f81d7505bb1c8878e8351
Miguel Angel AcevedoGantiva <maacevedog2010@gmail.com>
2019-10-07 21:52:11 -0500

    Seeders arreglados

--- database/seeds/ValoresTableSeeder.php
+++ database/seeds/ValoresTableSeeder.php
@@ -17,17 +17,17 @@ class ValoresTableSeeder extends Seeder
             'id_valor_seguro' => 1
         ]);
         DB::table('valores')->insert([
-            'valor' => '5000',
+            'valor' => '10000',
             'id_rango_edad' => 1,
             'id_valor_seguro' => 2
         ]);
         DB::table('valores')->insert([
-            'valor' => '5000',
+            'valor' => '4000',
             'id_rango_edad' => 2,
             'id_valor_seguro' => 1
         ]);
         DB::table('valores')->insert([
-            'valor' => '5000',
+            'valor' => '8000',
             'id_rango_edad' => 2,
             'id_valor_seguro' => 2
         ]);
