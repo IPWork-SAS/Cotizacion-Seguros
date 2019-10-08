f3ddce06155bc6d99e0d80acb3a5f391ed7c46d7
Duvan Gonzalez <programmerloquillo@gmail.com>
2019-10-08 12:14:55 -0500

    Se añaden estilos a la vista de verificación del codigo

--- resources/views/formularios/validacion.blade.php
+++ resources/views/formularios/validacion.blade.php
@@ -1,10 +1,18 @@
-<div>
-    {{$codigo}}
-    <form action="/cotizacionvalidar" method="POST">
+@extends('layouts.header')
+@section('titulo')
+    <b>Verificación de código ({{$codigo}})</b>
+@endsection
+@section('contenido')
+    <form action="/cotizacionvalidar" method="POST" class="wufoo leftLabel hideMarkers cotizacion">
         @csrf
-        <label for="codigo"></label>
-        <input type="text" name="codigo">
-        <input type="submit" value="Verificar">
-
+        <div class="row">
+            <label class="desc">
+                Código:
+            </label>
+            <input name="codigo" type="text" class="field text large" value="" size="8" tabindex="0" required="" style="text-align: center;">
+        </div>
+        <div style="text-align: center; margin-top: 25px;">
+            <button type="submit">Verificar</button>
+        </div>
     </form>
-</div>
+@endsection
